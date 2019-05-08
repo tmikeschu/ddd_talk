684dec9b6f5b7a7496652c749688e406ec1a9a37
Mike Schutte <tmikeschutte@gmail.com>
2019-05-08 09:36:24 -0400

    Update cafe code

--- lib/cafe.rb
+++ lib/cafe.rb
@@ -1,11 +1,20 @@
 class Cafe
-  attr_reader :available_cups_of_coffee
+  attr_reader :available_cups_of_coffee,
+              :chai_leaves_count,
+              :available_cups_of_chai
 
   def initialize
     @available_cups_of_coffee = 0
+    @chai_leaves_count        = 10
+    @available_cups_of_chai   = 0
   end
 
   def make_coffee
     @available_cups_of_coffee += 16
+  end
+
+  def make_tea
+    @chai_leaves_count      -= 1
+    @available_cups_of_chai += 1
   end
 end
--- spec/cafe_spec.rb
+++ spec/cafe_spec.rb
@@ -12,4 +12,20 @@ RSpec.describe Cafe do
       expect(cups_after - cups_before).to eq 16
     end
   end
+
+  describe '#make_tea' do
+    it 'makes 1 cup of chai tea and available leaves goes down by one' do
+      cafe = Cafe.new
+      chai_leaves_before = cafe.chai_leaves_count
+      cups_before = cafe.available_cups_of_chai
+
+      cafe.make_tea
+
+      chai_leaves_after = cafe.chai_leaves_count
+      cups_after = cafe.available_cups_of_chai
+
+      expect(chai_leaves_before - chai_leaves_after).to eq 1
+      expect(cups_after - cups_before).to eq 1
+    end
+  end
 end
